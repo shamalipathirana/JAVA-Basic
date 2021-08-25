f1dd51d774ea7b49711f943b6ca13bb41e79484d
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 12:31:29 +0530

    If else

---
+++ Lesson6If-Else/src/lesson6if/pkgelse/Lesson6IfElse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson6if.pkgelse;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson6IfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int time = 20;
+       if(time <18){
+           System.out.println("Good day");
+       }
+       else{
+           System.out.println("Good Evening");
+       }
+    }
+    
+}
---
+++ Lesson6Switch/src/lesson6switch/Lesson6Switch.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson6switch;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson6Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int day = 4;
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+        }
+    }
+    
+}
