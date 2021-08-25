a231d6347522666c09ab0b70153b550a653fc3ff
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 08:08:28 +0530

    Display sentence

---
+++ lesson1-Hello world/src/lesson1/hello/world/Lesson1HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson1.hello.world;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson1HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello sha.Now You are going to learn java");
+    }
+    
+}
