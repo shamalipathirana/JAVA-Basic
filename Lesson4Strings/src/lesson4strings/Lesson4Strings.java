6320627a0fbd231984585479474bd2e72bad5ccb
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 10:06:57 +0530

    Java String

---
+++ Lesson4Strings/src/lesson4strings/Lesson4Strings.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson4strings;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson4Strings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(txt);
+    }
+    
+}
