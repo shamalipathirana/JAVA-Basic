d3820ca3a645a178d3307a1b36805acc2b806ef8
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 15:55:30 +0530

    Created 2D array

---
+++ Lesson10MultidimensionalArray/src/lesson10multidimensionalarray/Lesson10MultidimensionalArray.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson10multidimensionalarray;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson10MultidimensionalArray {
+
+    public static void main(String[] args) {
+        int[][] a ={{1,2,3},{4,5,6},{7,8,9}};
+        
+        System.out.println("Length of row 1:"+a[0].length );
+    }
+    
+}
