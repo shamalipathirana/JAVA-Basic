78a55720c9405c288feba87aab50a4d35a74668c
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 12:54:45 +0530

    Create method()

---
+++ Lesson7Methods/src/lesson7methods/Lesson7Methods.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson7methods;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson7Methods {
+
+    static void myMethod(){
+        System.out.println("I am shamali");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+    
+}
