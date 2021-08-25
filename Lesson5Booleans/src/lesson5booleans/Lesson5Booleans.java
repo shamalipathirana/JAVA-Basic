c68926689c39600321bda1e59ec7386add23a6dd
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-25 11:22:26 +0530

    Boolean

---
+++ Lesson5Booleans/src/lesson5booleans/Lesson5Booleans.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson5booleans;
+
+/**
+ *
+ * @author SHAMALI
+ */
+public class Lesson5Booleans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean isJavaFun=true;
+        boolean isFishTasty=false;
+        
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+    }
+    
+}
